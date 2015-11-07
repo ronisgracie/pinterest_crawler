13ad0bd0aa5994494b2253e93c9a5b71353a7dfd
ronisgracie <ronis@gracieoc.com>
2015-11-07 15:26:59 -0800

    changes do the crawler

--- crawler.py
+++ crawler.py
@@ -4,22 +4,19 @@ from splinter import Browser
 class PinterestCrawler(object):
 
 
-	def __init__(self, url, profile):
+	def __init__(self, url):
 		self.navigator = Browser()
 		self.url = url
 		#self.profile = 'urldoprofileaqui'
 
 	def login_to_pinterest(self, login, password):
-		url = 'http://www.pinterest.com/login/'
-		navigate = navigator.visit(url)
-		
 		#variables to the account
 		email_input = login
 		password_input = password
 		#variables to access input
-		emailLogin = navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginUsername > input')[0]
-		emailPassword = navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginPassword > input')[0]
-		loginButton = navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > div.formFooter > div > button')[0]
+		emailLogin = self.navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginUsername > input')[0]
+		emailPassword = self.navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginPassword > input')[0]
+		loginButton = self.navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > div.formFooter > div > button')[0]
 		
 		#inputting the info
 		emailLogin.fill(email_input)
