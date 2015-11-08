4aa336b0d2502df720db454e8403e15c18f29f97
ronisgracie <ronis@gracieoc.com>
2015-11-07 19:41:17 -0800

    changes do crawler

--- crawler.py
+++ crawler.py
@@ -9,14 +9,15 @@ class PinterestCrawler(object):
 		self.url = url
 		#self.profile = 'urldoprofileaqui'
 
+
 	def login_to_pinterest(self, login, password):
 		#variables to the account
 		email_input = login
 		password_input = password
 		#variables to access input
-		emailLogin = self.navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginUsername > input')[0]
-		emailPassword = self.navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginPassword > input')[0]
-		loginButton = self.navigate.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > div.formFooter > div > button')[0]
+		emailLogin = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginUsername > input')[0]
+		emailPassword = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginPassword > input')[0]
+		loginButton = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > div.formFooter > div > button')[0]
 		
 		#inputting the info
 		emailLogin.fill(email_input)
