97de4b5d5871882b3dcfd6931ae9c7bc2181390a
ronisgracie <ronis@gracieoc.com>
2015-11-08 13:01:56 -0800

    abc

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ start = PinterestCrawler('http://www.pinterest.com/login/')
 
 access_website = start.navigator.visit(start.url)
 
-get_login = start.login_to_pinterest('graciewestwood@gmail.com', 'blackhouse123')
+get_login = start.login_to_pinterest('', '')
 
 start.clickfollow('https://www.pinterest.com/ntmcats/followers/')
