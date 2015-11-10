1e164c470135fb637d9292e48c5948315282f576
ronisgracie <ronis@gracieoc.com>
2015-11-09 19:16:57 -0800

    crawler improvements, added main method to the class

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+clean:
+	find . -name "*.pyc" -exec rm -rf {} \;
+
 shell:
 	ipython
+
+run: clean
+	python crawler.py
--- crawler.py
+++ crawler.py
@@ -1,41 +1,54 @@
+#!/usr/bin/python
+# unicode: utf-8
+
+import time
+import random
 from splinter import Browser
-import re
+
 
 class PinterestCrawler(object):
 
+    def __init__(self, url):
+        self.navigator = Browser()
+        self.url = url
 
-	def __init__(self, url):
-		self.navigator = Browser()
-		self.url = url
-		#self.profile = 'urldoprofileaqui'
+    def login_to_pinterest(self, login, password):
+        email_input = login
+        password_input = password
+        emaillogin = self.navigator.find_by_css(
+            'body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginUsername > input')[0]
+        emailpassword = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginPassword > input')[0]
+        loginbutton = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > div.formFooter > div > button')[0]
 
+        emaillogin.fill(email_input)
+        emailpassword.fill(password_input)
+        return loginbutton.click()
 
-	def login_to_pinterest(self, login, password):
-		#variables to the account
-		email_input = login
-		password_input = password
-		#variables to access input
-		emailLogin = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginUsername > input')[0]
-		emailPassword = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > ul > li.loginPassword > input')[0]
-		loginButton = self.navigator.find_by_css('body > div > div.appContent > div.mainContainer > div > div > div > form > div.formFooter > div > button')[0]
-		
-		#inputting the info
-		emailLogin.fill(email_input)
-		emailPassword.fill(password_input)
-		return loginButton.click()
+    def clickfollow(self, url):
+        self.navigator.visit(url)
+        time.sleep(3)
+        self.navigator.execute_script(
+            'window.scrollTo(50,document.body.scrollHeight);')
 
-	def clickfollow(self, url):
-		self.navigator.visit(url)
-		css_path = 'div.App.AppBase.Module.full > div.appContent > div.mainContainer > div.Module.UserProfilePage > div.Module.UserProfileContent > div > div > child(1) > div > button'
-		for child in css_path:
-			number = re.findall('[0-9+]', css_path)
-			child = number.pop(0)
-			new_child = int(child)
-			while new_child <= 50:
-				self.navigator.find_by_css(css_path)[0].click()
-				new_child +=1
-				css_path.replace(child, str(new_child))
-			else: break
+        css_path = 'body > div.App.AppBase.Module.full > div.appContent > div.mainContainer > div.Module.UserProfilePage > div.Module.UserProfileContent > div > div > div:nth-child({0}) > div > button'
+        new_child = 1
+        while new_child <= 50:
+            css_path2 = css_path.format(new_child)
+            self.navigator.find_by_css(css_path2)[0].click()
+            new_child += 1
+            time.sleep(random.randint(3, 12))
 
+    def main(self):
+        self.navigator.visit(self.url)
+        self.login_to_pinterest('', '')
 
-#body > div.App.AppBase.Module.full > div.appContent > div.mainContainer > div.Module.UserProfilePage > div.Module.UserProfileContent > div > div >
+        self.clickfollow('https://www.pinterest.com/ntmcats/followers/')
+
+start = PinterestCrawler('http://www.pinterest.com/login/')
+
+if __name__ == '__main__':
+    print 'ok'
+    start.main()
+else:
+    print dir(start.navigator)
+    start.navigator.quit()
